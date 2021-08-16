a8bdeabc538c6ecf3d487c5fc74117bf3bcf17cb
ipatidar <ipatidar@DIN17001288.corp.capgemini.com>
2021-08-16 13:33:32 +0530

    Adding salesclass file

--- firstdemo/src/main/java/firstdemo/HelloWorld.java
+++ firstdemo/src/main/java/firstdemo/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Goof afternoon");
+		System.out.println("Good afternoon");
 		System.out.println("Welcome to git");
 		
 	}
