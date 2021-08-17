7073a45d8f41d1f33d37bb28be8dd67050b63b51
ipatidar <ipatidar@DIN17001288.corp.capgemini.com>
2021-08-17 12:41:50 +0530

    Updated salescalss file

--- firstdemo/src/main/java/firstdemo/SalesClass.java
+++ firstdemo/src/main/java/firstdemo/SalesClass.java
@@ -5,13 +5,14 @@ public class SalesClass {
 	public void display() {
 		int[] arr= {1,2,3,4,5};
 		int sum=0;
-		for(int num:arr) {
-			sum+=num;
-			System.out.println(num);
+		for(int i=0; i<arr.length;i++) {
+			sum+=arr[i];
+			System.out.println(arr[i]);
 		}
 		System.out.println("Summation: "+sum);
 	}
 	public void print() {
+		System.out.println("Array size");
 		System.out.println("Print all array elements");
 		display();
 	}
