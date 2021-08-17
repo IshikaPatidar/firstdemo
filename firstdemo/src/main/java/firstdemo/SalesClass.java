8a0c9b19e902a3d05681a2f9ee0bd703e2e79adc
ipatidar <ipatidar@DIN17001288.corp.capgemini.com>
2021-08-17 12:59:42 +0530

    adding more code in salesclass

--- firstdemo/src/main/java/firstdemo/SalesClass.java
+++ firstdemo/src/main/java/firstdemo/SalesClass.java
@@ -5,14 +5,15 @@ public class SalesClass {
 	public void display() {
 		int[] arr= {1,2,3,4,5};
 		int sum=0;
-		for(int i=0; i<arr.length;i++) {
-			sum+=arr[i];
-			System.out.println(arr[i]);
+		for(int num:arr) {
+			sum+=num;
+			System.out.println(num);
 		}
 		System.out.println("Summation: "+sum);
 	}
 	public void print() {
-		System.out.println("Array size");
+		
+		System.out.println("Adding some more code to create conflict");
 		System.out.println("Print all array elements");
 		display();
 	}
